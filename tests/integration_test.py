157c9e47660fd045700fc5dd188bf0d2f8e2accb
Oscar Eriksson <scorch@stink>
2017-12-04 07:12:53 +0800

    calculating concordancy locally instead of uploading

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -6,6 +6,7 @@ import os
 import logging
 import time
 import traceback
+from uuid import uuid4 as uuid
 
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(sys.argv[0])))
@@ -41,20 +42,74 @@ logging.basicConfig(
 logger = logging.getLogger('integration_test')
 upload_executor = ThreadPoolExecutor(max_workers=10)
 clf_executor = ThreadPoolExecutor(max_workers=clf_n_jobs)
+submission_executor = ThreadPoolExecutor(max_workers=2)
+
+
+class NumeraiApiWrapper(NumerAPI):
+    def __init__(self, public_id=None, secret_key=None, verbosity="INFO"):
+        super(NumeraiApiWrapper, self).__init__(public_id, secret_key, verbosity)
+        self.checked = set()
+        self.cluster_ids = dict()
+        self.clusters = dict()
+        self.futures = dict()
+
+    def set_data(self, tournament_data, training_data):
+        self.cluster_ids = {
+            'test': tournament_data[tournament_data.data_type == 'test'].id.copy().values.ravel(),
+            'valid': tournament_data[tournament_data.data_type == 'validation'].id.copy().values.ravel(),
+            'live': tournament_data[tournament_data.data_type == 'live'].id.copy().values.ravel(),
+        }
+        self.clusters = get_competition_variables_from_df(
+            '1', training_data, tournament_data,
+            self.cluster_ids['valid'], self.cluster_ids['test'], self.cluster_ids['live'])
+
+    def upload_predictions(self, file_path):
+        sub_id = str(uuid())
+        self.futures[sub_id] = submission_executor.submit(self.check_concordance, file_path)
+        return sub_id
+
+    def check_concordance(self, submission_file_path):
+        submission = pd.read_csv(submission_file_path)
+        ids_valid, ids_test, ids_live = self.cluster_ids['valid'], self.cluster_ids['test'], self.cluster_ids['live']
+        p1, p2, p3 = get_sorted_split(submission, ids_valid, ids_test, ids_live)
+        c1, c2, c3 = self.clusters['cluster_1'], self.clusters['cluster_2'], self.clusters['cluster_3']
+        has_it = has_concordance(p1, p2, p3, c1, c2, c3)
+        # logger.info('submission %s has concordance? %s' % (submission_file_path, str(has_it)))
+        return has_it
+
+    def submission_status(self, submission_id=None):
+        if submission_id not in self.futures:
+            raise ValueError('unknown submission id %s' % submission_id)
+
+        f = self.futures.get(submission_id)
+        if not f.done():
+            pending = True
+            value = False
+        else:
+            pending = False
+            value = f.result()
+
+        return {
+            'originality': {
+                'pending': pending,
+                'value': value
+            },
+            'concordance': {
+                'pending': pending,
+                'value': value
+            }
+        }
 
 
 def main():
     # when running on circleci, set the vars in the project settings
-    public_id = os.environ.get('NUMERAPI_PUBLIC_ID')
-    secret_key = os.environ.get('NUMERAPI_SECRET_KEY')
-
-    if public_id is None or secret_key is None:
-        raise RuntimeError('set the NUMERAPI_PUBLIC_ID and NUMERAPI_SECRET_KEY environment variables first')
-
-    napi = NumerAPI(public_id=public_id, secret_key=secret_key)
+    public_id = os.environ.get('NUMERAPI_PUBLIC_ID', '')
+    secret_key = os.environ.get('NUMERAPI_SECRET_KEY', '')
 
     if not os.path.exists(test_csv):
         os.makedirs(test_csv)
+
+    napi = NumeraiApiWrapper(public_id=public_id, secret_key=secret_key)
 
     if not os.path.exists(DATA_SET_PATH):
         logger.info("Downloading the current dataset...")
@@ -66,23 +121,14 @@ def main():
     training_data = pd.read_csv('%s/numerai_training_data.csv' % DATA_SET_PATH, header=0)
     tournament_data = pd.read_csv('%s/numerai_tournament_data.csv' % DATA_SET_PATH, header=0)
 
+    napi.set_data(tournament_data, training_data)
+
     features = [f for f in list(training_data) if "feature" in f]
     features = features[:len(features)//2]  # just use half, speed things up a bit
     X, Y = training_data[features], training_data["target"]
 
     x_prediction = tournament_data[features]
     ids = tournament_data["id"]
-
-    # TODO: check concordance locally maybe
-    """
-    cluster_ids = {
-        'test': tournament_data[tournament_data.data_type == 'test'].id.copy().values.ravel(),
-        'valid': tournament_data[tournament_data.data_type == 'validation'].id.copy().values.ravel(),
-        'live': tournament_data[tournament_data.data_type == 'live'].id.copy().values.ravel(),
-    }
-    clusters = get_competition_variables_from_df(
-        '1', training_data, tournament_data, cluster_ids['valid'], cluster_ids['test'], cluster_ids['live'])
-    """
 
     clfs = [
         RandomForestClassifier(
@@ -111,44 +157,72 @@ def main():
     logger.info('all mix clfs predict_proba() took %.2fs' % (time.time()-before))
 
     legit_submission_ids = list()
+    mix_submission_ids = list()
+
     before = time.time()
     for f in futures.as_completed(uploads_wait_for_legit):
-        logger.info('future done, result: %s' % str(f))
         legit_submission_ids.append(f.result())
     logger.info('await legit uploads took %.2fs' % (time.time() - before))
 
+    before = time.time()
+    for f in futures.as_completed(uploads_wait_for_mix):
+        mix_submission_ids.append(f.result())
+    logger.info('await mix uploads took %.2fs' % (time.time() - before))
+
+    n_passed_originality, n_passed_concordance = get_originality_and_concordance(napi, legit_submission_ids)
+    if len(n_passed_originality) != len(clfs):
+        logger.error('legit passed originality %s/%s' % (len(n_passed_originality), len(clfs)))
+        sys.exit(1)
+    if len(n_passed_concordance) != len(clfs):
+        logger.error('legit passed concordance %s/%s' % (len(n_passed_concordance), len(clfs)))
+        sys.exit(1)
+
+    n_passed_originality, n_passed_concordance = get_originality_and_concordance(napi, mix_submission_ids)
+    if len(n_passed_originality) > 0:
+        logger.error('mix passed originality %s/%s' % (len(n_passed_originality), len(clfs)))
+        sys.exit(1)
+    else:
+        logger.info('all legit tests passed!')
+
+    if len(n_passed_concordance) > 0:
+        logger.error('mix passed concordance %s/%s' % (len(n_passed_concordance), len(clfs)))
+        sys.exit(1)
+    else:
+        logger.info('all mix tests passed!')
+
+    sys.exit(0)
+
+
+def get_originality_and_concordance(napi, _submission_ids):
+    submission_ids = _submission_ids.copy()
+    n_passed_originality = set()
+    n_passed_concordance = set()
+
     while True:
         statuses = list()
-        for submission_id in legit_submission_ids:
-            statuses.append(upload_executor.submit(napi, check_status, submission_id))
+        for submission_id in submission_ids:
+            statuses.append(upload_executor.submit(check_status, napi, submission_id))
 
         check_later = list()
         for f in futures.as_completed(statuses):
+            submission_id = f.result()['id']
             originality = f.result()['result']['originality']
-            if originality['pending']:
+            concordance = f.result()['result']['concordance']
+
+            if originality['pending'] or concordance['pending']:
                 check_later.append(f.result()['id'])
-                continue
-            if not originality['value']:
-                logger.error('submission %s failed originality: %s' % (f.result()['id'], str(f.result())))
-                sys.exit(1)
+            if originality['value']:
+                n_passed_originality.add(submission_id)
+            if concordance['value']:
+                n_passed_concordance.add(submission_id)
 
         if len(check_later) == 0:
             break
 
-        legit_submission_ids.clear()
-        legit_submission_ids = check_later.copy()
+        submission_ids.clear()
+        submission_ids = check_later.copy()
 
-    before = time.time()
-    for f in futures.as_completed(uploads_wait_for_mix):
-        logger.info('future done, result: %s' % str(f))
-    logger.info('await mix uploads took %.2fs' % (time.time() - before))
-
-
-def check_concordance(submission, clusters, ids):
-    ids_valid, ids_test, ids_live = ids['valid'], ids['test'], ids['live']
-    p1, p2, p3 = get_sorted_split(submission, ids_valid, ids_test, ids_live)
-    c1, c2, c3 = clusters['cluster_1'], clusters['cluster_2'], clusters['cluster_3']
-    return has_concordance(p1, p2, p3, c1, c2, c3)
+    return n_passed_originality, n_passed_concordance
 
 
 def check_status(napi, submission_id):
@@ -222,7 +296,7 @@ def upload_one_legit(y_prediction, ids, out: str, napi):
         # TODO: when api fixed, add the submission_id to  exc pool, async checks status to; if any fails: sys.exit(1)
         # input("Both concordance and originality should pass. Press enter to continue...")
         response = napi.upload_predictions(out)
-        logger.info('upload response: %s' % str(response))
+        # logger.info('upload response: %s' % str(response))
         return response
     except Exception as e:
         logger.exception(traceback.format_exc())
@@ -239,10 +313,21 @@ def predict_and_upload_mix(napi, clfs: list, tournament_data: pd.DataFrame, x_pr
     wait_for = list()
     uploads_wait_for = list()
 
+    checked_combos = set()
+
     for i, clf1 in enumerate(clfs[:len(clfs)//2]):
         for j, clf2 in enumerate(clfs[:len(clfs)//2]):
             if i == j:
                 continue
+
+            name_1 = str(clf1).split("(")[0] + '-' + str(clf2).split("(")[0]
+            name_2 = str(clf2).split("(")[0] + '-' + str(clf1).split("(")[0]
+
+            if name_1 in checked_combos or name_2 in checked_combos:
+                continue
+
+            checked_combos.add(name_1)
+            checked_combos.add(name_2)
 
             wait_for.append(clf_executor.submit(predict_and_upload_one_mix, napi, uploads_wait_for, clf1, clf2, x_pv, x_pt, ids_v, ids_t))
 
@@ -281,8 +366,9 @@ def upload_one_mix(napi, out, ids_v, ids_t, y_pt, y_pv):
         before_upload = time.time()
         response = napi.upload_predictions(out)
         time_taken = '%.2fs' % (time.time() - before_upload)
-        logger.info('upload took   %s%s (%s)' % (time_taken, ' '*(10-len(time_taken)), str(response)))
+        # logger.info('upload took   %s%s (%s)' % (time_taken, ' '*(10-len(time_taken)), str(response)))
         # input("Concordance should fail. Press enter to continue...")
+        return response
     except Exception as e:
         logger.exception(traceback.format_exc())
         logger.error('error uploading: %s' % str(e))
